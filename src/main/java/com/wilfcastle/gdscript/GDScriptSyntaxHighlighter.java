a98ede22ce3485aace845f3663bb2d25c8b33505
suri <swigder@gmail.com>
2018-12-19 23:45:40 -0500

    more syntax highlighting. rename test file to use correct extension.

--- src/main/java/com/wilfcastle/gdscript/GDScriptSyntaxHighlighter.java
+++ src/main/java/com/wilfcastle/gdscript/GDScriptSyntaxHighlighter.java
@@ -1,6 +1,5 @@
 package com.wilfcastle.gdscript;
 
-import com.wilfcastle.gdscript.psi.GDScriptTypes;
 import com.intellij.lexer.Lexer;
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
 import com.intellij.openapi.editor.HighlighterColors;
@@ -8,22 +7,39 @@ import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
 import com.intellij.psi.TokenType;
 import com.intellij.psi.tree.IElementType;
+import com.wilfcastle.gdscript.psi.GDScriptTypes;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey;
 
 public class GDScriptSyntaxHighlighter extends SyntaxHighlighterBase {
-    public static final TextAttributesKey COMMENT =
-            createTextAttributesKey("GDScript_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
-    public static final TextAttributesKey NUMBER =
-            createTextAttributesKey("GDScript_NUMBER", DefaultLanguageHighlighterColors.NUMBER);
-    public static final TextAttributesKey BAD_CHARACTER =
-            createTextAttributesKey("GDScript_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
 
-    private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
-    private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
-    private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
-    private static final TextAttributesKey[] NUMBER_KEYS = new TextAttributesKey[]{NUMBER};
+    private static final TextAttributesKey[] EMPTY_KEYS =
+            new TextAttributesKey[0];
+
+    private static final TextAttributesKey[] BAD_CHAR_KEYS =
+            createKeys(HighlighterColors.BAD_CHARACTER);
+
+    private static final TextAttributesKey[] COMMENT_KEYS =
+            createKeys(DefaultLanguageHighlighterColors.LINE_COMMENT);
+
+    private static final TextAttributesKey[] NUMBER_KEYS =
+            createKeys(DefaultLanguageHighlighterColors.NUMBER);
+
+    private static final TextAttributesKey[] KEYWORD_KEYS =
+            createKeys(DefaultLanguageHighlighterColors.KEYWORD);
+
+    private static final TextAttributesKey[] OPERATOR_KEYS =
+            createKeys(DefaultLanguageHighlighterColors.OPERATION_SIGN);
+
+    private static final TextAttributesKey[] IDENTIFIER_KEYS =
+            createKeys(DefaultLanguageHighlighterColors.IDENTIFIER);
+
+    private static final List<IElementType> KEYWORDS = Arrays.asList(GDScriptTypes.VAR);
+    private static final List<IElementType> OPERATORS = Arrays.asList(GDScriptTypes.EQUALS);
 
     @NotNull
     @Override
@@ -36,13 +52,24 @@ public class GDScriptSyntaxHighlighter extends SyntaxHighlighterBase {
     public TextAttributesKey[] getTokenHighlights(IElementType tokenType) {
         if (tokenType.equals(GDScriptTypes.COMMENT)) {
             return COMMENT_KEYS;
+        } else if (tokenType.equals(GDScriptTypes.IDENTIFIER)) {
+            return IDENTIFIER_KEYS;
         } else if (tokenType.equals(TokenType.BAD_CHARACTER)) {
             return BAD_CHAR_KEYS;
         } else if (tokenType.equals(GDScriptTypes.NUMBER)) {
             return NUMBER_KEYS;
+        } else if (KEYWORDS.contains(tokenType)) {
+            return KEYWORD_KEYS;
+        } else if (OPERATORS.contains(tokenType)) {
+            return OPERATOR_KEYS;
         } else {
             return EMPTY_KEYS;
         }
+    }
+
+    private static TextAttributesKey[] createKeys(TextAttributesKey key) {
+        return new TextAttributesKey[]{createTextAttributesKey(
+                key.toString().replace("DEFAULT", "GDScript"), key)};
     }
 }
 
